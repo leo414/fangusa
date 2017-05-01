e37541a144a48f47b09e1bea03d6c529eec79802
zaxlct <zaxlct@foxmail.com>
2017-05-01 23:04:36 +0800

    Add scaffolding for scss

--- src/main.js
+++ src/main.js
@@ -2,32 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 
-import iView from 'iview'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
-import VueResource from 'vue-resource'
 
 import App from './App'
 import Routers from './router'
 import Util from './libs/util'
 
-import  './scss/index.scss'
+import  './scss/utility.scss'
 import 'iview/dist/styles/iview.css'
 
-Vue.use(iView)
-Vue.use(VueResource)
+import LoadingBar from 'iview/src/components/loading-bar'
+
+
 Vue.use(Vuex)
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
-
-Vue.http.interceptors.push(function(request, next) {
-  request.method = 'GET'
-  request.headers.set('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8;')
-  next()
-})
-
-Vue.http.options.emulateJSON = true
 
 // route config
 const RouterConfig = {
@@ -37,13 +28,13 @@ const RouterConfig = {
 const router = new VueRouter(RouterConfig)
 
 router.beforeEach((to, from, next) => {
-  iView.LoadingBar.start()
+  LoadingBar.start()
   Util.title(to.meta.title)
   next()
 })
 
 router.afterEach((to, from, next) => {
-  iView.LoadingBar.finish()
+  LoadingBar.finish()
   window.scrollTo(0, 0)
 })
 
@@ -51,6 +42,5 @@ router.afterEach((to, from, next) => {
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)
 })
