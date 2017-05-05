c446492b68d58d31d56406dca3ad62a5643041d2
zaxlct <zaxlct@foxmail.com>
2017-05-05 11:39:35 +0800

    BannerComponent

---
+++ src/components/homePage/Banner.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+  <Carousel id="banner" autoplay v-model="value2">
+    <Carousel-item>
+      <div class="demo-carousel">1</div>
+    </Carousel-item>
+    <Carousel-item>
+      <div class="demo-carousel">2</div>
+    </Carousel-item>
+    <Carousel-item>
+      <div class="demo-carousel">3</div>
+    </Carousel-item>
+    <Carousel-item>
+      <div class="demo-carousel">4</div>
+    </Carousel-item>
+  </Carousel>
+</template>
+
+<script>
+import Carousel from 'iview/src/components/carousel'
+const CarouselItem = Carousel.Item
+
+export default {
+  name: 'Banner',
+  components: {
+    Carousel,
+    CarouselItem,
+  },
+  data() {
+    return {
+      value2: 0,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#banner {
+  width: 100%;
+  height: 500px;
+  line-height: 500px;
+}
+
+.demo-carousel {
+  width: inherit;
+  height: inherit;
+  text-align: center;
+  color: #fff;
+  font-size: 20px;
+  background: #506b9e;
+}
+</style>
--- src/components/homePage/index.vue
+++ src/components/homePage/index.vue
@@ -1,11 +1,16 @@
 <template lang="html">
-<section>
-  homwpage
-</section>
+<div id="home_page">
+  <banner-layout />
+</div>
 </template>
 
 <script>
+import Banner from './Banner.vue'
 export default {
+  name: 'HomePage',
+  components: {
+    bannerLayout: Banner,
+  }
 }
 </script>
 
