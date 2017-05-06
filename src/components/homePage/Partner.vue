42773da3bbaae18fe473e0dae2a07e4646269b37
zaxlct <zaxlct@foxmail.com>
2017-05-06 11:17:28 +0800

    PartnerComponent

---
+++ src/components/homePage/Partner.vue
@@ -0,0 +1,71 @@
+<template lang="html">
+<div class="container" id="partner">
+  <h1 class="h1">
+    合作伙伴
+  </h1>
+  <Row class="partner_list" :gutter="8">
+    <Coll span="6">
+      <div class="item"></div>
+    </Coll>
+    <Coll span="6">
+      <div class="item"></div>
+    </Coll>
+    <Coll span="6">
+      <div class="item"></div>
+    </Coll>
+    <Coll span="6">
+      <div class="item"></div>
+    </Coll>
+  </Row>
+
+  <Row class="partner_list" :gutter="8">
+    <Coll span="6">
+      <div class="item"></div>
+    </Coll>
+    <Coll span="6">
+      <div class="item"></div>
+    </Coll>
+    <Coll span="6">
+      <div class="item"></div>
+    </Coll>
+    <Coll span="6">
+      <div class="item"></div>
+    </Coll>
+  </Row>
+</div>
+</template>
+
+<script>
+import { Row, Col } from 'iview/src/components/grid'
+
+export default {
+  name: 'Partner',
+  components: {
+    Row,
+    Coll: Col,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#partner {
+  margin-top: 60px;
+}
+
+.partner_list {
+  margin-top: 20px;
+  height: 160px;
+
+  &:last-child {
+    margin-top: 8px;
+  }
+
+  .item {
+    height: 160px;
+    background: url('http://placehold.it/256x160') center center no-repeat;
+    background-size: cover;
+  }
+}
+
+
+</style>
