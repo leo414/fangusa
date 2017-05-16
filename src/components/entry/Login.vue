1590f4b0eeac379fac408cb6501b431460d0cbc1
zaxlct <zaxlct@foxmail.com>
2017-05-16 16:07:27 +0800

    登录页面

--- src/components/entry/Login.vue
+++ src/components/entry/Login.vue
@@ -1,6 +1,32 @@
 <template>
-<section id="Login">
-login
+<section class="entry_container">
+  <img src="../../assets/logo.png" alt="LOGO" class="header logo">
+
+  <div class="form">
+    <div class="input_box">
+      <input type="text" class="input" placeholder="手机号 / 邮箱" />
+    </div>
+
+    <div class="input_box">
+      <input type="password" class="input" placeholder="请输入密码" />
+    </div>
+
+    <mt-button class="btn submit" @click.native="handleClick" size="large" type="primary">登录</mt-button>
+  </div>
+
+  <div class="divide">
+    <hr class="hr" />
+    <p class="divide_text">其他登录方式</p>
+  </div>
+
+  <mt-button class="btn wexin" @click.native="handleClick" size="large" type="primary">微信登录</mt-button>
+
+  <footer class="footer">
+    <router-link class="forget_pd" to="/forget_password">忘记密码？</router-link>
+    <div class="fr">
+      <router-link to="/register">尚未注册？现在去注册</router-link>
+    </div>
+  </footer>
 </section>
 
 </template>
@@ -16,8 +42,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-@import "../../scss/variables";
-
-</style>
