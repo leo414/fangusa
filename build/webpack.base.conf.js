59bf2a423fe32151bf3053e0f9da324af368a594
zaxlct <zaxlct@foxmail.com>
2017-06-24 15:18:56 +0800

    Add path alias

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,10 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': resolve('src'),
+      'components': resolve('src/components'),
+      'lib': resolve('src/lib'),
+      'views': resolve('src/views'),
     }
   },
   module: {
