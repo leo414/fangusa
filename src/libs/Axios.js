0ae4e074bfed344c3e6f58d363b93fcc1bf51c6d
zaxlct <zaxlct@foxmail.com>
2017-06-25 12:28:47 +0800

    Axios config

--- src/libs/Axios.js
+++ src/libs/Axios.js
@@ -1,5 +1,7 @@
 require('es6-promise').polyfill()
 import axios from 'axios'
+// import qs from 'qs'
+
 import Vue from 'vue'
 import { Message } from 'element-ui'
 
@@ -10,6 +12,18 @@ const Axios = axios.create({
   timeout: 10000,
   // withCredentials: true,  // CORS 配置，如果后端不用 CORS 请设置成 false ，或者删除本行
   // headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+})
+
+Axios.interceptors.request.use(config => {
+  // if(config.method  === 'post'){
+  //   // 传参序列化
+  //   // Only applicable for request methods 'PUT', 'POST', and 'PATCH'
+  //   config.data = qs.stringify(config.data);
+  // }
+  return config
+},(error) =>{
+  // 错误的传参 
+  return Promise.reject(error)
 })
 
 Axios.interceptors.response.use(res => {
