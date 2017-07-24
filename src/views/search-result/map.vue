b6fda29fd80c61d5e263bcabfba16f33c3d4bc77
zaxlct <zaxlct@foxmail.com>
2017-07-24 18:01:57 +0800

    Map-box-gl

--- src/views/search-result/map.vue
+++ src/views/search-result/map.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-
+import EarthQuakes from './earthquakes.json'
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
 mapboxgl.accessToken = 'pk.eyJ1IjoiemF4bGN0IiwiYSI6ImNqNHVsODAwNjBrcXAycXI1cTVkdGU3ZHEifQ.0QfeJeP1LfAbm64_bYxj6A'
 
@@ -27,8 +27,72 @@ export default {
   mounted() {
     const map = new mapboxgl.Map({
       container: 'map_box',
-      style: 'mapbox://styles/zaxlct/cj2iaf2o9002b2rmqr8brbsez'
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [-103.59179687498357, 40.66995747013945],
+      zoom: 4,
     })
+
+    map.on('load', function() {
+      map.addSource("earthquakes", {
+        type: "geojson",
+        data: EarthQuakes,
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+      });
+
+      map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "earthquakes",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": {
+            property: "point_count",
+            type: "interval",
+            stops: [
+              [0, "#51bbd6"],
+              [100, "#f1f075"],
+              [750, "#f28cb1"],
+            ]
+          },
+          "circle-radius": {
+            property: "point_count",
+            type: "interval",
+            stops: [
+              [0, 20],
+              [100, 30],
+              [750, 40]
+            ]
+          }
+        }
+      });
+
+      map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "earthquakes",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": "{point_count_abbreviated}",
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12
+        }
+      });
+
+      map.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "earthquakes",
+        filter: ["!has", "point_count"],
+        paint: {
+          "circle-color": "#11b4da",
+          "circle-radius": 4,
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#fff"
+        }
+      });
+    });
   }
 }
 </script>
