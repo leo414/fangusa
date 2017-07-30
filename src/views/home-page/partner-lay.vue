c8eb5019fc41112fd5bce10f6f3fd635a9ba300c
zaxlct <zaxlct@foxmail.com>
2017-07-30 21:23:32 +0800

    合作伙伴轮播方式

--- src/views/home-page/partner-lay.vue
+++ src/views/home-page/partner-lay.vue
@@ -1,14 +1,23 @@
 <template lang="html">
-<div class="container" id="partner" v-once>
+<div class="container" id="partner">
   <h2 class="h2">
     合作伙伴
   </h2>
-
-  <el-row class="partner_list" :gutter="8">
-    <el-col :span="6" v-for="(img, index) in imgList" :key="index">
-      <div class="item" :style="background(img)"></div>
-    </el-col>
-  </el-row>  
+  <el-carousel class="partner_list" indicator-position="outside" height="250px" :interval="2000" :autoplay="true">
+    <el-carousel-item v-for="(imgGroup, index) in imgListGroup" :key="index">
+      <el-row :gutter="20">
+        <el-col :span="6" v-for="(img, index2) in imgGroup" :key="index2">
+          <div
+            @mouseover="currentImgIndex = index2"
+            @mouseout="currentImgIndex = ''"
+            class="partner_img gray"
+            :class="currentImgIndex === index2 ? 'no_gray' : ''"
+            :style="background(img)">
+          </div>
+        </el-col>
+      </el-row>
+    </el-carousel-item>
+  </el-carousel>
 </div>
 </template>
 
@@ -31,12 +40,25 @@ export default {
         'MatterportLogo_Navy-b9d1ff8908576314972631116667878cd24eecea09fef907a050cd61760e78b1.png',
         'NTREIS-logo.png',
         'WeWork-Logo_copy.jpg',
-      ]
+      ],
+      currentImgIndex: '',
     }
   },
+  computed: {
+    imgListGroup() {
+      let data = this.imgList
+      let imgListGruop = []
+      while(data.length > 0) {
+        imgListGruop.push(data.splice(0, 4))
+      }
+      return imgListGruop
+    },
+  },
+
   methods: {
     background(img) {
        return {
+        backgroundColor: '#eee',
         backgroundImage: `url(${process.env.BASE_URL}media/logo/${img})`,
         backgroundSize: 'contain',
         backgroundRepeat: 'no-repeat',
@@ -52,18 +74,26 @@ export default {
   margin-top: 60px;
 }
 
+.gray {
+  -webkit-filter: grayscale(100%);
+  -moz-filter: grayscale(100%);
+  -ms-filter: grayscale(100%);
+  -o-filter: grayscale(100%);
+  filter: grayscale(100%);
+  filter: gray;
+}
+
+.no_gray {
+  filter: none !important;
+}
+
 .partner_list {
   margin-top: 20px;
   height: auto;
 
-  &:last-child {
-    margin-top: 8px;
-  }
-
-  .item {
-    height: 160px;
-    margin-bottom: 10px;
-    overflow: hidden;
+  .partner_img {
+    height: 300px;
   }
 }
+
 </style>
